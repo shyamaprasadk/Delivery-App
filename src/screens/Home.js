9b6a82808620dedab0ee85ba924780c151096d88
shyamaprasadk <shyamaprasad991@gmail.com>
2022-12-23 13:38:01 +0530

    navigation added

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Header from "../componets/Home/Header";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Header />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
